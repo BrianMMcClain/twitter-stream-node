7d23046270176d402dee6a846f43dc55dc667e58
Brian McClain <brianmmcclain@gmail.com>
2012-02-07 23:54:46 -0500

    Random testing of best transport

---
+++ test/https_text.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+var http = require('http');
+
+// Load config info
+var config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
+var auth = 'Basic ' + new Buffer(config.username + ':' + config.password).toString('base64');
+
+var https = require('https');
+
+var options = {
+  host: 'stream.twitter.com',
+  port: 443,
+  path: '/1/statuses/sample.json',
+  method: 'GET',
+  auth: config.username + ":" + config.password
+};
+
+var buffer = ""
+
+var req = https.request(options, function(res) {
+  console.log("statusCode: ", res.statusCode);
+  console.log("headers: ", res.headers);
+
+  res.on('data', function(d) {
+    buffer += d;
+	if (buffer.indexOf('\n') != -1) {
+		// New Tweet
+		tmp = buffer.split('\n');
+		for (var i = 0; i < tmp.length-1; i++) {
+			var tweet = JSON.parse(tmp[i]);
+			if (tweet.user) {
+				console.log(tweet.user.screen_name);
+			}
+		}
+		buffer = tmp[tmp.length - 1];
+	}
+  });
+});
+req.end();
+
+req.on('error', function(e) {
+  console.error(e);
+});
--- test/request_stream.js
+++
@@ -1,8 +0,0 @@
-var request = require('request');
-var fs = require('fs');
-
-// Load config info
-var config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
-
-
-request.get("https://" + config.username + ":" + config.password + "@stream.twitter.com/1/statuses/sample.json").pipe(fs.createWriteStream('/dev/stdout'));
