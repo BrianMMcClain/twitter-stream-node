99d0c51184500251cf5016ab73e1afe212434f39
Brian McClain <brianmmcclain@gmail.com>
2012-03-05 00:10:45 -0500

    Updated single tweet limit test

--- test/lib_test.js
+++ test/lib_test.js
@@ -4,6 +4,6 @@ var stream = require('../lib/stream.js');
 
 var config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
 
-stream.public_limit(config.username, config.password, 3, function(tweet) {
-	console.log(tweet.text);
+stream.public_limit(config.username, config.password, 1, function(tweet) {
+	console.log(tweet);
 });
