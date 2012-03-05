47c314111ceab2542d607e2fe910e2adaa13ee5d
Brian McClain <brianmmcclain@gmail.com>
2012-03-05 00:17:53 -0500

    HTTPS request closes after limit is hit

--- test/lib_test.js
+++ test/lib_test.js
@@ -5,5 +5,5 @@ var stream = require('../lib/stream.js');
 var config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
 
 stream.public_limit(config.username, config.password, 1, function(tweet) {
-	console.log(tweet);
+	console.log(tweet.entities.user_mentions);
 });
