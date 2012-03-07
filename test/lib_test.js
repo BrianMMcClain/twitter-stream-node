3aa2e7f3a1cb1b62f83b1b37a316d815f2d44cd0
Brian McClain <brianmmcclain@gmail.com>
2012-03-06 19:45:49 -0500

    Continued testing

--- test/lib_test.js
+++ test/lib_test.js
@@ -7,9 +7,5 @@ var config = JSON.parse(fs.readFileSync("./config.json", "utf-8"));
 // Testing limited stream
 stream.public_limit(config.username, config.password, 3, function(tweet) {
 	console.log(tweet.text);
-});
-
-// Testing unlimited streaming
-stream.public(config.username, config.password, function(tweet) {
-	console.log(tweet.text);
+	console.log("\n");
 });
